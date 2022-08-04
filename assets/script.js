a024f72f5d936d7e4d097e3628c91478fd6835fb
Andrew S <andrewsales2004@gmail.com>
2022-08-03 22:44:06 -0500

    created character arrays, created loop if response to password length is not within parameters and added window.confirm prompts for uppercase, lowercase, special and numerical characters

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,36 @@
 // Assignment code here
+
+//Create the Arrays
+var Upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var Lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var Special = ["\\","\b","\r","\"","\f","\n","\'","\t"]
+var Numbers = ["0","1","2","3","4","5","6","7","8","9"]
+
 // Declare the variables
 var passLength = "";
-
-
+var UpCase;
+var LwCase;
+var SpcChar;
+var NmChar;
 
 // Define generatePassword
 function generatePassword() {
   console.log("Button is now working.");
 // Add prompts when button is clicked pertaining to password criteria
   // Length of password between 8-128 characters
-  var passLength = (prompt("How many character, between 8 and 128, would you like in you password?"))
+  var passLength = (window.prompt("How many character, between 8 and 128, would you like in you password?"))
+    // Loop prompt if response is not within specified parameters
+    while(passLength <= 7 || passLength >= 129) {
+      var passLength = (window.prompt("How many character, between 8 and 128, would you like in you password?"))
+    }
   // Will uppercase, lowercase, numbers and/or special characters be used?
+  var UpCase = (window.confirm("Click OK to confirm including uppercase characters."))
+
+  var LwCase = (window.confirm("Click OK to confirm including lowercase characters."))
+
+  var SpcChar = (window.confirm("Click OK to confirm including special characters."))
+
+  var NmChar = (window.confirm("Click OK to confirm including numerical characters."))
 //Validate input and at least one character of each selected criteria is used
 
 //Display generated password(return)
