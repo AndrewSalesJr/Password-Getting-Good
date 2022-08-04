77cf2dc06a2aa23a5b2e8add2bc360466943ef66
Andrew S <andrewsales2004@gmail.com>
2022-08-03 21:14:49 -0500

    added variable declaration, prompt pseudo code, and initial, basic password lenght prompt

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,26 @@
 // Assignment code here
+// Declare the variables
+var passLength = "";
+
+
+
+// Define generatePassword
+function generatePassword() {
+  console.log("Button is now working.");
+// Add prompts when button is clicked pertaining to password criteria
+  // Length of password between 8-128 characters
+  var passLength = (prompt("How many character, between 8 and 128, would you like in you password?"))
+  // Will uppercase, lowercase, numbers and/or special characters be used?
+//Validate input and at least one character of each selected criteria is used
+
+//Display generated password(return)
+
+  return "Placeholder Password!"
+}
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); 
-
-function generatePassword() {
-  console.log("Button is now working.");
-
-  return "Password!"
-}
 
 // Write password to the #password input
 function writePassword() {
